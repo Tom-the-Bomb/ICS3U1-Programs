a6cf44a43a154696121d47d31d74b62306bb9400
Tom-the-Bomb <thello522@gmail.com>
2023-11-03 23:21:09 -0400

    initial commit

---
+++ face-java.java
@@ -0,0 +1,60 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Face extends Canvas {
+    private static int width = 600;
+    private static int height = 850;
+    public static void main(String[] args) {
+        Face face = new Face();
+        JFrame frame = new JFrame("House");
+
+        frame.setSize(width, height);
+        frame.getContentPane()
+            .setBackground(Color.white);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(face);
+        frame.setVisible(true);
+    }
+
+    public void paint(Graphics g) {
+        Font font = new Font("Arial", Font.BOLD, 40);
+        g.setFont(font);
+
+        // face + ears
+        g.setColor(new Color(254, 254, 0));
+        g.fillOval(20, 50, width - 40, height - 50);
+        g.fillOval(0, width / 2 + 75, 20, 100);
+        g.fillOval(width - 20, width / 2 + 75, 20, 100);
+        // mouth
+        g.setColor(new Color(160, 160, 0));
+        g.fillOval(width / 2 - 75, height - 200, 150, 40);
+        // nose
+        g.setColor(new Color(0, 176, 240));
+        g.fillRect(width / 2 - 15, height - 440, 30, 180);
+        // eyes
+        g.setColor(Color.white);
+        g.fillOval(width / 2 - 140, height - 540, 100, 80);
+        g.fillOval(width / 2 + 60, height - 540, 100, 80);
+
+        g.setColor(Color.black);
+        g.drawString("Face", width / 2 - 20, 40);
+        // face
+        g.drawOval(20, 50, width - 40, height - 50);
+        // ears
+        g.drawOval(0, width / 2 + 75, 20, 100);
+        g.drawOval(width - 20, width / 2 + 75, 20, 100);
+        // mouth
+        g.drawOval(width / 2 - 75, height - 200, 150, 40);
+        // nose
+        g.drawRect(width / 2 - 15, height - 440, 30, 180);
+        // eyes
+        g.drawOval(width / 2 - 140, height - 540, 100, 80);
+        g.drawOval(width / 2 + 60, height - 540, 100, 80);
+        // pupils
+        g.fillOval(width / 2 - 110, height - 525, 40, 50);
+        g.fillOval(width / 2 + 90, height - 525, 40, 50);
+        // eyebrows
+        g.fillRect(width / 2 - 140, height - 590, 110, 20);
+        g.fillRect(width / 2 + 60, height - 590, 110, 20);
+    }
+}
---
+++ house-java.java
@@ -0,0 +1,60 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class House extends Canvas {
+    private static int width = 1518;
+    private static int height = 850;
+    public static void main(String[] args) {
+        House house = new House();
+        JFrame frame = new JFrame("House");
+
+        frame.setSize(width, height);
+        frame.getContentPane()
+            .setBackground(Color.white);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(house);
+        frame.setVisible(true);
+    }
+
+    public void paint(Graphics g) {
+
+        Font font = new Font("Arial", Font.BOLD, 40);
+        g.setFont(font);
+        g.setColor(Color.black);
+        g.drawString("House", width / 2 - 20, 40);
+
+        int bgHeight = height - 50;
+        g.setColor(new Color(126, 95, 0));
+        g.fillRect(0, 50, width, bgHeight);
+
+        int triBase = 50 + bgHeight / 3;
+        g.setColor(Color.black);
+        int[] triX = {0, width / 2, width};
+        int[] triY = {triBase, 50, triBase};
+        g.drawPolygon(triX, triY, 3);
+
+        int rectHeight = height - triBase;
+        int doorX = 2 * (width / 5);
+        int doorY = triBase + rectHeight / 3;
+        int doorWidth = width / 5;
+        int doorHeight = 2 * (rectHeight / 3);
+        int doorMid = doorX + doorWidth / 2;
+
+        g.setColor(new Color(254, 254, 0));
+        g.fillRect(width / 5, triBase, 3 * (width / 5), rectHeight);
+        g.setColor(new Color(255, 0, 0));
+        g.fillRect(doorX, doorY - 40, doorWidth, 40);
+        g.setColor(new Color(0, 112, 192));
+        g.fillRect(doorX, doorY, doorWidth, doorHeight);
+
+        g.setColor(Color.black);
+        // house
+        g.drawRect(width / 5, triBase, 3 * (width / 5), rectHeight);
+        // door
+        g.drawRect(doorX, doorY, doorWidth, doorHeight);
+        // door red top
+        g.drawRect(doorX, doorY - 40, doorWidth, 40);
+        // door middle line
+        g.drawLine(doorMid, doorY, doorMid, height);
+    }
+}
