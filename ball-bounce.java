edf7fcb1356efbb45a19b3f19beb13d620d08d46
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2023-11-09 11:21:00 -0500

    Create ball-bounce.java

---
+++ ball-bounce.java
@@ -0,0 +1,48 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MovingBall extends JPanel implements ActionListener {
+    int x = 0;
+    int y = 0;
+    Timer timer = new Timer(10, this);
+    
+    int velX = 2; // x-velocity px/s
+    int velY = 3; // y-velocity px/s
+    
+    static int width = 500; // width of box
+    static int height = 400; // height of box
+    static int rad = 50; // radius of ball
+    
+    public static void main(String[] args) {
+        MovingBall movingBall = new MovingBall();
+        JFrame frame = new JFrame("Animation");
+        frame.setSize(width, height);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(movingBall);
+    }
+    
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(new Color(10, 10, 10));
+        g.drawRect(0, 0, width, height);
+        
+        g.setColor(Color.red);
+        g.fillOval(x, y, rad, rad);
+        timer.start();
+    }
+    
+    public void actionPerformed(ActionEvent event) {
+        x += velX;
+        y += velY;
+        repaint();
+        
+        if (x >= width - rad || x <= 0) {
+            velX *= -1;
+        }
+        if (y >= height - rad || y <= 0) {
+            velY *= -1;
+        }
+    }
+}
