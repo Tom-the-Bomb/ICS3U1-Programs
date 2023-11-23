96549238af3cf017beb2d8321bf9ca466774f73f
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2023-11-23 11:41:28 -0500

    Update ArrayMethods.java

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -21,6 +21,35 @@ public class ArrayMethods
         return newArr;
     }
     
+    /* Pops the element at the end of the array */
+    public static<T> T[] popBack(T[] arr) {
+        T[] newArr = Arrays.copyOf(arr, arr.length - 1);
+        for (int i = 1; i < arr.length; i++) {
+            newArr[i - 1] = arr[i];
+        }
+        return newArr;
+    }
+    
+    /* Pops the element at the front of the array */
+    public static<T> T[] popFront(T[] arr) {
+        T[] newArr = Arrays.copyOf(arr, arr.length - 1);
+        return newArr;
+    }
+    
+    /* Pops the element of the array at specified index */
+    public static<T> T[] remove(T[] arr, int index) {
+        T[] newArr = Arrays.copyOf(arr, arr.length - 1);
+        int m = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i == index) {
+                m = 1;
+                continue;
+            }
+            newArr[i - m] = arr[i];
+        }
+        return newArr;
+    }
+    
     /* Inserts `element` into index `index` of the array */
     public static<T> T[] insert(T[] arr, int index, T element) {
         T[] newArr = Arrays.copyOf(arr, arr.length + 1);
@@ -43,16 +72,45 @@ public class ArrayMethods
         return arr;
     }
     
-    /* Bubble sort algorithm */
-    public static<T extends Comparable<? super T>> T[] bubbleSort(T[] arr) {
+    /* Bubble sort algorithm with `reverse` argument */
+    public static<T extends Comparable<? super T>> T[] bubbleSort(T[] arr, boolean reverse) {
         for (int t = 0; t < arr.length; t++) {
             for (int i = 1; i < arr.length - t; i++) {
-                if (arr[i - 1].compareTo(arr[i]) > 0) {
+                if ((reverse && arr[i - 1].compareTo(arr[i]) < 0)
+                    || (!reverse && arr[i - 1].compareTo(arr[i]) > 0)
+                ) {
                     swap(arr, i - 1, i);
                 }
             }
         }
         return arr;
+    }
+    
+    /* Bubble sort algorithm */
+    public static<T extends Comparable<? super T>> T[] bubbleSort(T[] arr) {
+        return bubbleSort(arr, false);
+    }
+    
+    /* Returns the maximum value within the array */
+    public static<T extends Comparable<? super T>> T max(T[] arr) {
+        T maximum = arr[0];
+        for (T element : arr) {
+            if (element.compareTo(maximum) > 0) {
+                maximum = element;
+            }
+        }
+        return maximum;
+    }
+    
+    /* Returns the minimum value within the array */
+    public static<T extends Comparable<? super T>> T min(T[] arr) {
+        T minimum = arr[0];
+        for (T element : arr) {
+            if (element.compareTo(minimum) < 0) {
+                minimum = element;
+            }
+        }
+        return minimum;
     }
     
     /* Returns the index of an element in an array, returns -1 if element does not exist */
@@ -82,20 +140,20 @@ public class ArrayMethods
     public static void main(String[] args) {
         String[] arr1 = {"a", "b", "c", "d"};
         printArr(pushFront(arr1, "!"));
+        printArr(pushBack(arr1, "!"));
+        printArr(insert(arr1, 2, "!"));
         
-        String[] arr2 = {"a", "b", "c", "d"};
-        printArr(pushBack(arr2, "!"));
+        printArr(popFront(arr1));
+        printArr(popBack(arr1));
+        printArr(remove(arr1, 2));
         
-        String[] arr3 = {"a", "b", "c", "d"};
-        printArr(insert(arr3, 2, "!"));
+        System.out.println("Index of \"c\": " + indexOf(arr1, "c"));
+        printArr(reverse(arr1));
         
-        Integer[] arr4 = {4, 2, 5, 1};
-        printArr(bubbleSort(arr4));
-        
-        String[] arr5 = {"a", "b", "c", "d"};
-        System.out.println(indexOf(arr5, "c"));
-        
-        String[] arr6 = {"a", "b", "c", "d"};
-        printArr(reverse(arr6));
+        Integer[] arr2 = {4, 2, 5, 1};
+        printArr(bubbleSort(arr2));
+        printArr(bubbleSort(arr2, true));
+        System.out.println("Minimum: " + max(arr2));
+        System.out.println("Maximum: " + min(arr2));
     }
 }
